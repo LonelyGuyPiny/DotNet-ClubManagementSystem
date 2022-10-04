ecc65672dd996e3e53d98458e3cd1ab038e705e4
LonelyGuyPiny <lonelyguypiny@gmail.com>
2022-10-04 15:59:15 +0300

    Profile menu component

--- src/Shared/MainLayout.razor.cs
+++ src/Shared/MainLayout.razor.cs
@@ -1,10 +1,6 @@
-﻿using ClubManagementSystem.Data.Entities;
-using ClubManagementSystem.Extensions;
-using ClubManagementSystem.Resources;
+﻿using ClubManagementSystem.Resources;
 using ClubManagementSystem.Theme;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Localization;
 using MudBlazor;
 
@@ -16,15 +12,6 @@ namespace ClubManagementSystem.Shared
         protected DefaultTheme DefaultTheme { get; set; } = new();
         protected bool IsDrawerOpened { get; set; }
         protected bool IsDarkMode { get; set;}
-        protected string? FullName { get; set; }
-        protected string? Role { get; set; }
-        protected char? FirstLetterOfFirstName { get; set; }
-
-        [Inject]
-        protected AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
-
-        [Inject]
-        protected UserManager<User> UserManager { get; set; } = null!;
 
         [Inject]
         protected IStringLocalizer<Common> Localizer { get; set; } = null!;
@@ -41,20 +28,6 @@ namespace ClubManagementSystem.Shared
             {
                 IsDarkMode = await MudThemeProvider.GetSystemPreference();
                 StateHasChanged();
-            }
-        }
-
-        protected override async Task OnInitializedAsync()
-        {
-            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-            var userId = authState.User.GetUserId();
-            var isAdmin = authState.User.IsInRole(Roles.Admin);
-            if (userId != null)
-            {
-                var user = await UserManager.FindByIdAsync(userId);
-                FirstLetterOfFirstName = user?.FirstName?[0];
-                FullName = user?.FullName;
-                Role = isAdmin ? Roles.Admin : string.Empty;
             }
         }
     }
